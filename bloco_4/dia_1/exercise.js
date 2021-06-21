8d0a8d531ce3d2f0ad6724c0a0ebeef15b48a1ee
brenoMatias <matiasbreno19@gmail.com>
2021-06-21 13:23:06 -0300

    Atualização exercicios, até 7

--- bloco_4/dia_1/exercise.js
+++ bloco_4/dia_1/exercise.js
@@ -33,3 +33,40 @@ if ( n3 > n4 && n3 >n5) {
 else {
     console.log("O maior número é " + n5);
 }
+
+let var1 = "negative";
+
+if (var1 == "positive") {
+    console.log("positive");
+  } else if (var1 == "negative") {
+    console.log("negative");
+  } else {
+    console.log("invalid iput");
+  }
+
+  let ang1 = 90;
+  let ang2 = 60;
+  let ang3 = 60;
+  let somaAng = ang1 + ang2 + ang3
+  console.log("Soma angulos internos " + somaAng)
+  
+  if (ang1 < 0 || ang2 < 0  || ang2 < 0) {
+    console.log("inavalid input");
+  }
+    if (somaAng > 180 || somaAng < 180){
+      console.log("false");
+  } else if (somaAng = 180){
+      console.log("true");
+  } else {
+      console.log("invalid iput");
+  }
+   
+let chessPiece = "Bishop";
+chessPiece = chessPiece.toLowerCase()
+  if (chessPiece == "pawn" || chessPiece == "bishop" || chessPiece == "knight") {
+      console.log("Peça válida");
+  }else {
+      console.log("peça invalida")
+  }
+  
+  
