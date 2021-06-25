48f253c4930640282d8268a1b8d7776e5e840887
brenoMatias <matiasbreno19@gmail.com>
2021-06-25 11:48:26 -0300

    exercicio 2 - for/array/function

--- bloco_4/dia_4/exercises-function.js
+++ bloco_4/dia_4/exercises-function.js
@@ -2,21 +2,33 @@ let string = "Ana";
 
 function validatePalin(word) {  
   word = word.toLowerCase();
-  // get the total length of the words  
-  let len = word.length;  
+  let len = word.length;   // get the total length of the words
   
-  for (let i = 0; i < len; i++) {   // iterate through the string
-
-      // validate the first and last characters are same  
-      if (word[i] !== word[len - 1 - i]) {  
-          return false;  
+  for (let i = 0; i < len; i += 1) {   // iterate through the string 
+      if (word[i] !== word[len - 1 - i]) {  //validate the first and last characters are same  
+          return false;   // se for falso já para na primeira iteração
       }  
   }  
   return true;  
 }  
 console.log(validatePalin(string));
 
-let testArray = [2, 3, 6, 7, 10, 1];
-function maxInArray(){
-    let arrayLen = 
+
+ 
+let testArray = [200, 3, 6, 70, 10, 1];  
+
+function maxInarray(testArray) {
+   
+    let max = testArray[0];  // Initialize maximum element
+  
+    for (let i = 0; i < testArray.length; i += 1) { // Traverse array elements and compare
+        if (testArray[i] > max) {                 // every element with current max 
+            max = testArray[i];
+    }
 }
+return max;
+  
+}
+
+console.log(maxInarray(testArray));
+
