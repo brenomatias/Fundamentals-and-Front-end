06f5e28b309a3d1ec7be6193c9ecb4936aea805d
brenoMatias <matiasbreno19@gmail.com>
2021-06-28 19:26:26 -0300

    exercício 1

--- bloco_5/dia_2/script.js
+++ bloco_5/dia_2/script.js
@@ -1,3 +1,6 @@
-let local = document.createElement("P");
-local.innerText = "O local é Austrália";
-document.body.appendChild(local);
+//existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+//1 -Insert an element before the first child element,
+let parentElement = document.getElementById("pai");// Get the parent element
+let theFirstChild = parentElement.firstChild;// Get the parent's first child
+let newElement = document.createElement("teste"); // Create a new element
+pai.insertBefore(newElement, theFirstChild);
