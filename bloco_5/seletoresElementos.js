cf8570a9c9b3f01c018b9e15d72587447fd6873a
brenoMatias <matiasbreno19@gmail.com>
2021-06-28 11:31:43 -0300

    atualizações

--- bloco_5/seletoresElementos.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("page-title").innerText = "Clube da luta";
-// document.getElementById("paragraph").innerText = "testando seletores de elementos";
-// document.getElementById("second-paragraph").innerText = "teste";
-
-document.getElementsByClassName("paragraph")[0].style.color = "red";
-document.getElementById("page-title").style.color = "red";
