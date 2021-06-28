406aa6ffa2b4cec2cc4ef5324555222f614ce547
brenoMatias <matiasbreno19@gmail.com>
2021-06-28 19:41:08 -0300

    Exercício 2

--- bloco_5/dia_2/script.js
+++ bloco_5/dia_2/script.js
@@ -4,3 +4,9 @@ let parentElement = document.getElementById("pai");// Get the parent element
 let theFirstChild = parentElement.firstChild;// Get the parent's first child
 let newElement = document.createElement("teste"); // Create a new element
 pai.insertBefore(newElement, theFirstChild);
+
+//2- Crie um filho para elementoOndeVoceEsta .
+let elementoPai = document.getElementById("elementoOndeVoceEsta")
+let newNode = document.createElement("P");
+elementoPai.appendChild(newNode);
+newNode.innerText = "Teste"
