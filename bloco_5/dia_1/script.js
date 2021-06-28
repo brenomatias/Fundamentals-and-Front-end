deb318ff88b17017595305f4832f00466764438b
brenoMatias <matiasbreno19@gmail.com>
2021-06-28 16:13:20 -0300

    Finalização exercícios DOM dia 1

--- bloco_5/dia_1/script.js
+++ bloco_5/dia_1/script.js
@@ -9,4 +9,11 @@ document.getElementsByClassName("main-content")[0].style.backgroundColor = "gree
 //3. Crie uma função que mude a cor do quadrado vermelho para branco.
 document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
 
+//4. Crie uma função que corrija o texto da tag <h1>.
+document.getElementsByClassName("title")[0].innerText = "Exercício 5.1 - JavaEscripit";
 
+//5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+document.querySelectorAll("p")[1].style.textTransform = "uppercase";
+
+//  6. Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+document.querySelectorAll("p");
