419fde80c2986cd6b3e67d77d52476e181725d51
brenoMatias <matiasbreno19@gmail.com>
2021-07-03 22:58:54 -0300

    testes de criação de elementos javascript - click and change

--- bloco_5/dia_3/exercises5.3/script.js
+++ bloco_5/dia_3/exercises5.3/script.js
@@ -16,10 +16,10 @@
 
 
 //Exercício 1:
-const numberDay = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
 
   function createNumbersOfTheWeek() {
-    
+    const numberDay = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
     const numberDayList = document.querySelector('#days');
   
     for (let i = 0; i < numberDay.length; i += 1) {
@@ -46,15 +46,24 @@ const numberDay = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
  button.innerHTML = "Feriados";
  // add the button to the div
  myDiv.appendChild(button);
- button.setAttribute("id", "btn-holiday"); // set attribute https://stackoverflow.com/questions/19625646/javascript-adding-an-id-attribute-to-another-created-element
+ button.setAttribute("id", "btn-holiday"); // set attribute https://www.w3schools.com/jsref/met_element_setattribute.asp
 
  const holidayClick = document.getElementById("btn-holiday");
- holidayClick.addEventListener("click", recebeClick);
-
-
- function recebeClick (){ // referencia https://www.codegrepper.com/code-examples/javascript/javascript+append+html+string
-
- const parElement = document.getElementById("btn-holiday");
- const textToAdd = document.createTextNode("feriados");
- parElement.appendChild(textToAdd);
-}
+ holidayClick.addEventListener("click", receberClick2);
+ 
+ function receberClick2() {
+    const numberDay ="feriado"
+    const numberDayList = document.querySelector('#btn-holiday');
+  
+    for (let i = 0; i < numberDay.length; i += 1) {
+      days.classList.add("day"); // adiciona a classe day 
+      const numbers = numberDay[i];
+      const numbersListItem = document.createElement('li'); // cria os elementos de lista 'li'
+      numbersListItem.innerHTML = numbers;
+      numberDayList.appendChild(numbersListItem);
+    
+      if (numberDay[i] === 24 || numberDay[i] === 25 || numberDay[i] === 31 ){ // adiciona a classe holiday * note que o if está dentro do looping exterior pois pecorre toda a array;
+        numbersListItem.classList.add("holiday")
+    }
+    };
+  };
