2b388b268c68c8b432a2c47d8ed24db0a8b00300
brenoMatias <matiasbreno19@gmail.com>
2021-07-04 00:21:45 -0300

    Exercício 2

--- bloco_5/dia_3/exercises5.3/script.js
+++ bloco_5/dia_3/exercises5.3/script.js
@@ -40,30 +40,13 @@
   
  // Exercício 2  // https://www.codegrepper.com/code-examples/javascript/append+button+to+div+javascript referencia
 
- const myDiv = document.querySelector(".buttons-container"); 
- // create the button object and add the text to it
+
+ function selectHoliday(Feriados){
+ const myDiv = document.querySelector(".buttons-container");   // create the button object and add the text to it
  const button = document.createElement("button");
- button.innerHTML = "Feriados";
- // add the button to the div
+ button.innerHTML = "Feriados";  // add the button to the div
  myDiv.appendChild(button);
  button.setAttribute("id", "btn-holiday"); // set attribute https://www.w3schools.com/jsref/met_element_setattribute.asp
+}
 
- const holidayClick = document.getElementById("btn-holiday");
- holidayClick.addEventListener("click", receberClick2);
- 
- function receberClick2() {
-    const numberDay ="feriado"
-    const numberDayList = document.querySelector('#btn-holiday');
-  
-    for (let i = 0; i < numberDay.length; i += 1) {
-      days.classList.add("day"); // adiciona a classe day 
-      const numbers = numberDay[i];
-      const numbersListItem = document.createElement('li'); // cria os elementos de lista 'li'
-      numbersListItem.innerHTML = numbers;
-      numberDayList.appendChild(numbersListItem);
-    
-      if (numberDay[i] === 24 || numberDay[i] === 25 || numberDay[i] === 31 ){ // adiciona a classe holiday * note que o if está dentro do looping exterior pois pecorre toda a array;
-        numbersListItem.classList.add("holiday")
-    }
-    };
-  };
+selectHoliday();
