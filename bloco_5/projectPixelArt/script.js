e64de33f2e75291fa44954440152d420831134d3
brenoMatias <matiasbreno19@gmail.com>
2021-07-05 12:51:00 -0300

    Exercício 2 completo - estrutura sem edição

---
+++ bloco_5/projectPixelArt/script.js
@@ -0,0 +1,16 @@
+let color1 = document.getElementsByClassName("color")[0].style.border = " 1px solid black"; // Até rePadding é o exercício 2 
+let red = document.querySelector("#red").style.backgroundColor = "red";
+let redPadding = document.querySelector("#red").style.padding = "60px 60px";
+
+
+let color2 = document.getElementsByClassName("color")[1].style.border = " 1px solid black";
+let green = document.querySelector("#green").style.backgroundColor = "green";
+let greenPadding = document.querySelector("#green").style.padding = "60px 60px"
+
+let color3 = document.getElementsByClassName("color")[2].style.border = " 1px solid black";
+let yellow = document.querySelector("#yellow").style.backgroundColor = "yellow";
+let yellowPadding = document.querySelector("#yellow").style.padding = "60px 60px";  
+
+let color4 = document.getElementsByClassName("color")[3].style.border = " 1px solid black";
+let blue = document.querySelector("#blue").style.backgroundColor = "blue";
+let bluePadding = document.querySelector("#blue").style.padding = "60px 60px";
