ef63efbc13f4ccd4791fb515da992bfd8333ecb2
brenoMatias <matiasbreno19@gmail.com>
2021-07-06 12:52:04 -0300

    Requisito 5 - até parte de pegar input

--- bloco_5/projectToDoList/script.js
+++ bloco_5/projectToDoList/script.js
@@ -1,14 +1,35 @@
 // Requisito 3
 
 let input = document.querySelector(".input-task"); // seleciona o local de append child 
-let inputResize= document.createElement("input");
-inputResize.type = "text";
-input.id = "texto-tarefa";
-input.appendChild(inputResize);
+let inputTask= document.createElement("input");
+inputTask.type = "text";
+inputTask.id = "texto-tarefa";
+input.appendChild(inputTask);
+
 
 // Requisito 4
 
 let task = document.querySelector(".task-list"); // seleciona o local de append child 
 let taskList= document.createElement("ol");
-task.id = "texto-tarefa";
+taskList.id = "lista-tarefas";
 task.appendChild(taskList);
+
+// Requisito 5
+
+let button = document.querySelector(".button-input-task"); // seleciona o local de append child 
+let buttonInput= document.createElement("button");
+button.id = "criar-tarefa";
+buttonInput.innerText = "Adicionar tarefa"
+button.appendChild(buttonInput);
+
+
+ function addAnother() {
+    let text = inputTask.value;
+    var ul = document.getElementById("lista-tarefas");
+    var li = document.createElement("li");
+    li.innerText = text;
+    ul.appendChild(li)
+}
+
+buttonInput.addEventListener ("click", addAnother);
+
