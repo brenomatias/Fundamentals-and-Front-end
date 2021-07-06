7e62909ad7341809eb7c9fb29890bb7ce43bf19d
brenoMatias <matiasbreno19@gmail.com>
2021-07-06 11:38:14 -0300

    Requisito 4

--- bloco_5/projectToDoList/script.js
+++ bloco_5/projectToDoList/script.js
@@ -5,3 +5,10 @@ let inputResize= document.createElement("input");
 inputResize.type = "text";
 input.id = "texto-tarefa";
 input.appendChild(inputResize);
+
+// Requisito 4
+
+let task = document.querySelector(".task-list"); // seleciona o local de append child 
+let taskList= document.createElement("ol");
+task.id = "texto-tarefa";
+task.appendChild(taskList);
