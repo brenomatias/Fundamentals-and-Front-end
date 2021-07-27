bb52bfb713f2219c131d5301958930ab769915d7
brenoMatias <matiasbreno19@gmail.com>
2021-07-27 11:10:30 -0300

    Requisito 2

--- bloco_7/projectUnitTests/script.js
+++ bloco_7/projectUnitTests/script.js
@@ -34,7 +34,7 @@ const assert = require('assert'); // The syntax for including the assert module 
 //   return false;
 // };
 
-const numbers = (array) => { // função que falha 
+const numbers = (array) => { // funçao que recebe a array como parâmetro
     for (let i = 0; i < array.length; i += 1) {
       if (typeof array[i] !== 'number') {
         return false;
@@ -43,9 +43,13 @@ const numbers = (array) => { // função que falha
     return true;
   };
 
-assert.strictEqual(numbers([1, 2, 3, 4, 5]), true, "teste 1 "); // Checks if two values are equal, using the strict equal operator (===)
-assert.strictEqual(numbers([1, 2, '3', 4, 5]), false, "teste 2"); // 
-assert.strictEqual(numbers([1, 'a', 3]), false, "teste 3");
-assert.strictEqual(numbers([' ']), false, "teste 4"); // message: Specifies the error message to be assigned to the AssertionError. If omitted, a default message is assigned
+assert.strictEqual(numbers([' ']), false, "teste vazio"); // Checks if two values are equal, using the strict equal operator (===)
+assert.strictEqual(numbers([10, 2, 3, 6, 5]), true, "teste array correta"); // message: Specifies the error message to be assigned
+// to the AssertionError. If omitted, a default message is assigned
+assert.strictEqual(numbers([5, 2, '3', 6]), false, "teste string"); 
+assert.strictEqual(numbers([1, 'a', 3]), false, "teste letter string"); // este teste também nao passa, mas nem chega nele porque 
+// quebra antes (no caso da função que não passa nos testes)
 
 module.exports = numbers;
+
+// Requisito 3
