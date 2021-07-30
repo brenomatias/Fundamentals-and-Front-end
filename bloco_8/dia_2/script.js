1a9c688814ef31f3a3df7592e902c7bd12a1e665
brenoMatias <matiasbreno19@gmail.com>
2021-07-29 22:01:16 -0300

    Inciando

---
+++ bloco_8/dia_2/script.js
@@ -0,0 +1,82 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+
+// 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+const authorBornIn1947 = books.find((authors) => authors.author.birthYear === 1947); // authors vai funcionar como parametro substituido por books
+console.log(authorBornIn1947);
+
+function getKeyByValue(object, value) {
+   let name = ''
+   object.find((key) => {
+      if (key.author.birthYear === value){
+      name 
+      } 
+
+  });
+}
+console.log(getKeyByValue(books, 1947));
+
