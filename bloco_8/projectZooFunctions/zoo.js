fc2332d70b0e491d8f5b1cfd3c8be3395b8dea2e
brenoMatias <matiasbreno19@gmail.com>
2021-08-04 11:11:04 -0300

    Até requisito 3

--- bloco_8/projectZooFunctions/zoo.js
+++ bloco_8/projectZooFunctions/zoo.js
@@ -1,16 +1,40 @@
 const data = require('./data');
+const { species, employees, prices, hours } = require('./data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+// function getSpeciesByIds(...ids) {
+//     const speciesById = species.filter((specie) => ids.includes(specie.id));
+//     return speciesById;
+function getSpeciesByIds(...ids) { 
+ let speciesArray = []; // define array de retorno
+ ids.forEach(element => speciesArray.push(species.find(specie => specie.id === element)));
+ return speciesArray;
+  }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce','533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'))
+// (...) Spread syntax can be used when all elements from an object or array need to be included in a list of some kin
+// The filter() method creates a new array with all elements that pass the test implemented by the provided function
+// vai filtrar as espécies de acordo com a condição de filter, que será a função include
+// The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate
+// realiza um filtro na array species e compara com os ids repassados (specie é referente a species na condição do filter)
+// se for true, cai na adição do filtro
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+ const nameAndAge = species.find(animalName => animalName.name === animal); // constante para armazenar o valor da chave name em species
+ return nameAndAge.residents.every(element => element.age > age);
 }
+//The find() method returns the value of the first element in the provided array that satisfies the provided testing function
+// o find vai localizar o animal pelo nome passado
+// residents e o subgrupo de species, por isso foi passado no nameAndAge;
+// o find é em cima do objeto species
+// The every() method tests whether all elements in the array pass the test implemented by the provided function
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+    if (employeeName === undefined) {
+      return {};
+    }
+    return employees.find((i) => i.firstName === employeeName || i.lastName === employeeName);
+  }
+  
+console.log(getEmployeeByName('Nigel'))
 
 function createEmployee(personalInfo, associatedWith) {
   // seu código aqui
