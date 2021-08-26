b3b886939c453ef36e0abad8422d3590c15e745b
brenoMatias <matiasbreno19@gmail.com>
2021-08-26 17:45:03 -0300

    Exercicio nao terminado

--- block11-react-introduction/dia_2/my-pokedex/src/App.js
+++ block11-react-introduction/dia_2/my-pokedex/src/App.js
@@ -1,11 +1,15 @@
+import { render } from '@testing-library/react';
 import './App.css';
+import pokemons from './data'; // importa os dados de data
+import Pokemon from './pokemon' // importa o componente pokemon
 
 function App() {
   return (
-    <h1 className="title">
-    Minha pokedex
-    </h1>
-  );
+    pokemons.map(pokemon => ( // função map vai gerar outra matriz com os dados separados
+      <div>
+        <Pokemon name={pokemon.name} type={pokemon.type} image={pokemon.image} />
+      </div>
+    )))
 }
 
 export default App;
