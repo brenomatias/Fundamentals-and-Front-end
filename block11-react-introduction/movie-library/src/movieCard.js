605bb69e8d1a3c82985a2fafbd8d938b610b88fe
brenoMatias <matiasbreno19@gmail.com>
2021-08-31 14:58:06 -0300

    atre req 10

--- block11-react-introduction/movie-library/src/movieCard.js
+++ block11-react-introduction/movie-library/src/movieCard.js
@@ -1,2 +1,17 @@
-import React from 'react';
 
+import React from 'react'; // JSX transform turns it into regular JavaScript:
+
+class MovieCard extends React.Component { // component estilo class
+    render() { // fator fundamental na construção de classe component
+        const { title, subtitle, storyLine, imagePath, rating } = this.props.movie; 
+
+        return ( 
+            <div className="movie">
+               <img src={imagePath} alt={`${title} sprite`} />
+               <h4> {title} </h4>
+               <h5> {subtitle} </h5>
+               <p> {storyLine} </p>
+            </div>
+        );
+    }
+}
