5b0d5f4b8139554a2af098fe7d8495c8e9abd31a
brenoMatias <matiasbreno19@gmail.com>
2021-09-13 06:03:43 -0300

    projeto

---
+++ block12-react-components-states/project/src/components/AddMovieComponents/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovieButton extends React.Component {
+  render() {
+    const { onClick } = this.props;
+    return (
+      <button
+        type="submit"
+        onClick={ onClick }
+        data-testid="send-button"
+      >
+        Adicionar filme
+      </button>
+    );
+  }
+}
+
+AddMovieButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default AddMovieButton;
