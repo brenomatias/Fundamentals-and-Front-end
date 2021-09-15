d043e548bf0d67c1a0a973359cd23e2c56ab1767
brenoMatias <matiasbreno19@gmail.com>
2021-09-15 18:24:54 -0300

    escreve explicaçao da aula e do codigo

--- block13-react-life-cycle-router/cronometer/src/App.js
+++ block13-react-life-cycle-router/cronometer/src/App.js
@@ -1,33 +1,37 @@
+// this code came from: https://github.com/tryber/sd-014-b-live-lectures/tree/lecture/13.1/cronometer/src
 import React from 'react';
 import './App.css';
 import Cronometer from './components/Cronometer';
 
 class App extends React.Component {
-  constructor() {
+  constructor() { // incializar as variável
     super();
 
-    this.state = {
-      showCronometer: true,
+    this.state = { // estado incial para para aparecer ou nao o cronometro
+      showCronometer: false,
     };
   }
 
-  toggleDisplayCronometer = () => {
-    this.setState((prevState) => ({
-      showCronometer: !prevState.showCronometer,
-    }));
-  }
+  // passo depois da condição ternária de "Cronometer"
+  toggleDisplayCronometer = () => { // função se de se exibe ou nao o valor
+    this.setState((prevState) => ({ // modifica o estado de showCronometer
+      showCronometer: !prevState.showCronometer, // modificar o estado com base no estado anterior (prevState)
+    })); // o estado recebe a negaçao "!" do estado anterior, ou seja, showCronometer seu valor de esado
+  } // se usa arrow function nao precisa do bind
 
   render() {
-    const { showCronometer } = this.state;
-    return (
+    const { showCronometer } = this.state; // acessar valor do estado definido
+    // desconstruido
+    return ( // tudo aqui pra baixo é JSX(nao e javaScript)
+    // apenas expressões, por isso o uso do ternário
       <main className="App">
         <section className="App-header">
-          { showCronometer ? <Cronometer /> : ''}
+          { showCronometer ? <Cronometer /> : ''} 
         </section>
 
         <button
           type="button"
-          onClick={ this.toggleDisplayCronometer }
+          onClick={ this.toggleDisplayCronometer } // recebe a função de remover o Cronometro
         >
           { showCronometer ? 'Desligar cronometro' : 'Ligar cronometro'}
         </button>
@@ -37,3 +41,15 @@ class App extends React.Component {
 }
 
 export default App;
+// Primeira parte: renderização condicional
+// Processo de exibir ou nao o cronometro
+// showCronometer: estado inicial definido (aparecer ou nao o component "Cronometer")
+// { showCronometer ? <Cronometer /> : ''}  condicional. retorna '' (vazio ) se for falso
+// operador ternário
+// JSX apenas chama expressoes 
+// {condiçao ? true : false}
+// se showCronomer ? o que exibir se for true(o que exibir se for verdadeiro) e false(o que exibir se for falso)
+// Passo após criar função toggleDisplayCronometer
+// { showCronometer ? 'Desligar cronometro' : 'Ligar cronometro'}
+// se showCronometer:true exibe 'Desligar cronometro'
+// se showCronometer:false exibe 'Desligar cronometro'
--- block13-react-life-cycle-router/cronometer/src/components/Cronometer.js
+++ block13-react-life-cycle-router/cronometer/src/components/Cronometer.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
 
-class Cronometer extends Component {
-  constructor(props) {
+class Cronometer extends Component { // iniciar variáveis
+  constructor(props) { // mais simples possivel 
     super(props);
-    this.state = { seconds: 0 };
+    this.state = { seconds: 0 }; // define o estado incial
+    // este estado definido "seconds" (seu valor) vai pra tela"
   }
 
-  componentDidMount() {
-    console.log('componentDidMount');
-    const ONE_SECOND = 1000;
+  componentDidMount() { // primeira função a ser definida -> dispara sozinho
+    // é chamado depois que o render() foi executado pela primeira vez
+    console.log('Montou');
+    const ONE_SECOND = 1000; // definição do valor do intervalo
+    // segundo parametro de setInterval
 
-    this.intervalID = setInterval(() => {
+    this.intervalID = setInterval(() => { // primeira função construida
+        // define variável dentro de DidAmount
+        // this para acessar setInterval fora de DidMount
+        // função recebe dois parametros (funçao, de quanto em quanto tempo)
       console.log('Atualizou o estado');
-      this.setState((prevState) => ({
-        seconds: prevState.seconds + 1,
+      this.setState((prevState) => ({ // atualiza o estado
+        // prevState = estado anterior de "seconds"
+        seconds: prevState.seconds + 1, // vai acrescentando +1
       }));
-    }, ONE_SECOND);
+    }, ONE_SECOND); 
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    const TIME_LIMIT = 10;
+  componentDidUpdate(prevProps, prevState) { // segundo passo, depois de DidMount
+    // atualiza o estado (acesso a prop anteriores, acessa estado anterior)
+    console.log('Atualizou')
+    const TIME_LIMIT = 10; // tempo maximo que vai o cronometro
     // const { seconds } = this.state;
-    if (prevState.seconds === TIME_LIMIT) {
-      // alert('Cabou o tempo');
-      this.setState({ seconds: 0 });
+    if (prevState.seconds === TIME_LIMIT) { // prevState acessa o estado anterior e compara
+      alert('Acabou o tempo');
+      this.setState({ seconds: 0 }); // define o valor do estado de seconds pra 0 se a condiçao é atingida
     }
-  }
+  } // este component é executado toda vez que o estado muda
 
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
+  componentWillUnmount() { // passso pos DidUpdate
+      // metodo ser ser executado quando componente for desmontado
+    clearInterval(this.intervalID); // parar intervalo (acessar o valor do ID do intervalo que é gerado em setInterval)
+  } // limpar o intervalo
 
   render() {
-    const { seconds } = this.state;
+    const { seconds } = this.state; // acessa o valor de seconds(valor do estado)
     return (
       <h2>
         {seconds}
@@ -42,3 +52,25 @@ class Cronometer extends Component {
 }
 
 export default Cronometer;
+// Ciclo de vida
+// Montagem:
+// constructor ()
+// render(); monta o DOM
+// componentDidMount(); é chamado dps do render -> aqui vai req api
+// Atualizaçoes:
+// novas props
+// mudança no estado
+// render() é chamado de novo: atualizar DOM
+// componentDidUpdate() é sempre chamado quando ha atualizações
+// Desmontagem
+// componentWillUnmount(); retirar do DOM
+// utilizar estruturas que o react tem, DidMount, WillMount -> da controle ao React
+// DidMount executa so uma vez
+// Ordem de execução
+// DidMount uma unica vez
+// executa setInterval()
+// componentDidUpdate
+// volta pra setInterval dps DidUpdate e assim vai
+// depois que chama render vai o DidUpdate
+// componentDidUpdate -> setState so pode ser incluido dentro de condiçoes
+// montou = renderizou
