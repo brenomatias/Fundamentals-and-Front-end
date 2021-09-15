7db993222ca2c8d271d9503b2aff1d48b0dbdf93
brenoMatias <matiasbreno19@gmail.com>
2021-09-15 20:22:55 -0300

    nao sei o que aconteceu

--- block13-react-life-cycle-router/trybe-clone/src/pages/Solutions.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Solutions extends Component {
-  render() {
-    return <h1>Solutions</h1>;
-  }
-}
-
-export default Solutions;
--- block13-react-life-cycle-router/trybe-clone/src/pages/TrybeTalks.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class TrybeTalks extends Component {
-  render() {
-    return <h1>TrybeTalks</h1>;
-  }
-}
-
-export default TrybeTalks;
