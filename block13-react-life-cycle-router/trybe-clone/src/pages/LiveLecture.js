2aa36e5dfdec028c2d649ae5135b706132f6f115
brenoMatias <matiasbreno19@gmail.com>
2021-09-15 20:43:44 -0300

    atualizaçao

---
+++ block13-react-life-cycle-router/trybe-clone/src/pages/LiveLecture.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class LiveLecture extends Component {
+  render() {
+    return <h1>LiveLecture</h1>;
+  }
+}
+
+export default LiveLecture;
---
+++ block13-react-life-cycle-router/trybe-clone/src/pages/Solutions.js
@@ -0,0 +1,10 @@
+
+import React, { Component } from 'react';
+
+class Solutions extends Component {
+  render() {
+    return <h1>Solutions</h1>;
+  }
+}
+
+export default Solutions;
