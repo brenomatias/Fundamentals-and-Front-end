3aa991855190a6c861fd21c16db8e7161a8eb083
brenoMatias <matiasbreno19@gmail.com>
2021-09-26 15:04:32 -0300

    inicia aula

--- block13-react-life-cycle-router/review-block11-12-13/src/App.js
+++ block13-react-life-cycle-router/review-block11-12-13/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Details from './pages/Details';
+import Main from './pages/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Main } />
+        <Route exact path="/details/:id" component={ Details } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
