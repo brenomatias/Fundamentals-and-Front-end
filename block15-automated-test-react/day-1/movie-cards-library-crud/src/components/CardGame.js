fdddc015cff2bfb7f0da0adf3d360667cac1c698
brenoMatias <matiasbreno19@gmail.com>
2021-09-27 15:00:27 -0300

    atualizacçoes

---
+++ block15-automated-test-react/day-1/movie-cards-library-crud/src/components/CardGame.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class CardGame extends React.Component {
+  render() {
+    const { game: { id, title, img, description } } = this.props;
+    return (
+      <div className='game-card'>
+        <img src={ img } alt={ title } />
+        <h3>{title}</h3>
+        <p className="game-card-description">{ description }</p>
+        <Link className="link" to={`details/${id}`}>Ver detalhes</Link>
+      </div>
+    )
+  }
+}
+
+export default CardGame;
